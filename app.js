fcb8f6b1a7deb220043dd54e9b154cc6dfcfbba8
Simon Oulevay <simon.oulevay@gmail.com>
2017-03-16 23:01:46 +0100

    Updated most dependencies

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
     express = require('express'),
-    favicon = require('serve-favicon'),
     logger = require('morgan'),
     multer = require('multer'),
     path = require('path');
@@ -16,12 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
