178180fd54968c76768265d9cd56e3168dae2880
Simon Pinkas <212782+simonpinkas@users.noreply.github.com>
2023-01-04 19:39:09 +0100

    Remove file duplicates in root

--- utils.js
+++
@@ -1,8 +0,0 @@
-export const sendError = (status, message, res, err) => {
-  if (err) console.log(err);
-  else console.error(message);
-  res.type("text/plain").status(status).send(message);
-};
-
-export const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
