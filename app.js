f74799994707e5b474882116d27f7e7fef3a31ba
Simon Pinkas <212782+simonpinkas@users.noreply.github.com>
2023-01-04 19:19:48 +0100

    Doc static serving

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use('/doc', express.static("doc"));
+
 app.use("/api/images", imageRouter);
 app.use("/api/tokens", tokenRouter);
 
