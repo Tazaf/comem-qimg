10e4f89f91c2f3f7095ca9550218229449427a9f
Simon Pinkas <212782+simonpinkas@users.noreply.github.com>
2023-01-04 19:38:30 +0100

    Add render URL to doc

--- upload.js
+++
@@ -1,8 +0,0 @@
-import { config } from "./config.js";
-import multer from "multer";
-
-const storage = multer.memoryStorage();
-export const upload = multer({
-  storage: storage,
-  limits: { fileSize: config.maxImageSize, files: 1 }
-});
